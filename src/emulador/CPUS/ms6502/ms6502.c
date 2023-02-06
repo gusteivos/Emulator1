e54fb89933647686419a49afd1abbec6baa2d01b
Gusteivos gp <gc5142387@gmail.com>
2023-02-06 10:46:09 -0300

    Fix include file

--- src/emulador/CPUS/ms6502/ms6502.c
+++ src/emulador/CPUS/ms6502/ms6502.c
@@ -7,21 +7,15 @@ MS6502 *CreateNewMS6502()
 
     if (_output_value == NULL) goto _end_of_function;
 
-    _output_value->a = 0x00;
-    _output_value->x = 0x00;
-    _output_value->y = 0x00;
+    _output_value->a    = 0x00;
+    _output_value->x    = 0x00;
+    _output_value->y    = 0x00;
 
-    _output_value->program_counter  = 0x0000;
-    _output_value->stack_pointer    = 0x00;
-    _output_value->status          |= MS6502_INTERRUPT_FLAG;
+    _output_value->pc   = 0x0000;
+    _output_value->sp   = 0x00;
+    _output_value->sr  |= MS6502_INTERRUPT_FLAG;
 
-    _output_value->fetched          = 0x00;
-    _output_value->temp             = 0x00;
-    _output_value->addr_abs         = 0x00;
-    _output_value->addr_rel         = 0x00;
-    _output_value->opcode           = 0x00;
-    _output_value->clock_count      = 0x00;
-    _output_value->clock_count      = 0x00;
+    
 
     _output_value->ReadMemory   = NULL;
 
@@ -36,10 +30,12 @@ _end_of_function:
 void ResetMS6502(MS6502 *cpu)
 {
     
-    cpu->program_counter = ((uint16_t)cpu->ReadMemory(0xFFFC) | (uint16_t)cpu->ReadMemory(0xFFFD) << 8);
+    cpu->pc     = 0xFFFC;
 
-    cpu->stack_pointer = 0xFF;
+    cpu->pc     = ((uint16_t)cpu->ReadMemory(cpu->pc) | (uint16_t)cpu->ReadMemory(0xFFFD) << 8);
 
-	cpu->status |= MS6502_INTERRUPT_FLAG;
+    cpu->sp     = 0xFF;
+
+	cpu->sr    |= MS6502_INTERRUPT_FLAG;
 
 }
